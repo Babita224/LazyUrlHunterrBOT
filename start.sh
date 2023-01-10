3d56a41e57c3462e83701aeca903eced4625dba8
Babita224 <122219261+Babita224@users.noreply.github.com>
2023-01-11 00:24:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/Babita224/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
 cd /LazyUrlHunterrBOT
-pip3 install -U -r requirements.txt
+pip3 install -r requirements.txt
 echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
 python3 main.py
